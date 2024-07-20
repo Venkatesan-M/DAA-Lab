9e1738531178aecd9dc9d0bc30dd63ba3531b45f
Venkatesan M <venkygaming18@gmail.com>
2024-07-20 09:28:27 +0000

    lab 1 code

--- Lab-1/code.cpp
+++ Lab-1/code.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <limits>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
-    vector<int> marks;
-    for (int i = 1; i <= 5; i++){
-        cout << "Enter the mark of student"<< endl;
-        int x;
-        cin >> x;
-        marks.push_back(x);
+struct Participant {
+    string name;
+    int score;
+};
+
+int main() {
+    int N;
+    cout << "Enter the number of participants: ";
+    cin >> N;
+
+    vector<Participant> participants(N);
+
+    for (int i = 0; i < N; ++i) {
+        cout << "Enter name and score for participant " << i + 1 << ": ";
+        cin >> participants[i].name >> participants[i].score;
     }
 
-    auto min = marks.begin(); 
-    auto max = marks.begin();
+    // Timing the execution
+    clock_t start = clock();
 
-    for (int i = marks.at(1); i != *marks.end(); ++i)
-        cout << i << " ";
+    auto minMaxParticipant = minmax_element(
+        participants.begin(), participants.end(),
+        [](const Participant &p1, const Participant &p2) {
+            return p1.score < p2.score;
+        });
+
+    Participant minParticipant = *minMaxParticipant.first;
+    Participant maxParticipant = *minMaxParticipant.second;
+
+    // Calculate execution time
+    double duration = (clock() - start) / (double)CLOCKS_PER_SEC;
+
+    cout << "Time taken to execute: " << duration << " seconds\n";
+    cout << "Minimum mark participant: " << minParticipant.name << " with score " << minParticipant.score << "\n";
+    cout << "Maximum mark participant: " << maxParticipant.name << " with score " << maxParticipant.score << "\n";
 
     return 0;
 }
